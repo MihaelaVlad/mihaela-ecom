c2ae956745eb61f8dfb3b19b5079d4a4a77b50e6
Mihaela Vlad <vlad.c.mihaela@gmail.com>
2022-09-27 10:02:59 +0300

    fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,18 +8,18 @@ const Home = () => {
         <title>Homepage</title>
       </Head>
 
-      <main className="bg-slate-900 h-screen flex flex-col justify-center items-center gap-y-20">
+      <main className="bg-slate-900 h-screen flex flex-col justify-center items-center gap-y-10">
         <h1 className="font-bold text-4xl text-center text-slate-100 hover:bg-sky-500 p-4 rounded-xl">
           Homepage
         </h1>
 
-        <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 hover:bg-slate-600 w-1/2">
+        <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 hover:bg-slate-600 w-1/2 text-center">
           <Image
-            className="w-24 h-24 object-cover rounded-full mx-auto"
+            className="object-cover rounded-full mx-auto"
             src="https://images.pexels.com/photos/13514939/pexels-photo-13514939.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="Profile picture"
-            height="24px"
-            width="24px"
+            height="124px"
+            width="124px"
           ></Image>
 
           <div className="pt-6 text-center space-y-4">
